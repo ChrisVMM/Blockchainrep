b13fa2eb04b0f97f129572aac08f0cdd19e53296
CVM2IBM <141054127+CVM2IBM@users.noreply.github.com>
2024-03-30 23:10:19 -0500

    update the header and make the profile page private

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,34 +1,45 @@
-import {FaSearch} from 'react-icons/fa'
-import { Link } from 'react-router-dom'
-export default function Header() {
-  return (
+import { FaSearch } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-      <header className='bg-slate-200 shadow-md'>
-          <div className='flex justify-between items-center max-w-6xl mx-auto'>
-        <h1 className='font-bold text-sm:text-xl flex flex-wrap'>
-          <span className='text-slate-500'>Blockchain</span>
-          <span className='text-slate-700'>Estate</span>
+export default function Header() {
+  const { currentUser } = useSelector(state => state.user);
+  
+  return (
+    <header className='bg-gray-800 border-b-4 border-gray-700 shadow-md'>
+      <div className='flex justify-between items-center max-w-6xl mx-auto'>
+        <h1 className='font-bold text-xl flex flex-wrap text-gray-200'>
+          <span>Blockchain</span>
+          <span className='text-gray-400'>Estate</span>
         </h1>
-        <form className='bg-slate-100 p-3 rounded-lg flex items-center'>
+        <form className='bg-gray-700 p-3 rounded-lg flex items-center'>
           <input 
-          type='text' 
-          placeholder='Search...' 
-          className='bg-transparent focus:outline-none w-24 sm:w-64'
+            type='text' 
+            placeholder='Search...' 
+            className='bg-transparent focus:outline-none w-24 sm:w-64 text-gray-300'
           />
-          <FaSearch className='text-slate-600'/>
+          <FaSearch className='text-gray-400' />
         </form>
         <ul className='flex gap-4'>
-          <Link to='/'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+          <Link to='/' className='text-gray-300 hover:underline'>
+            <li>Home</li>
           </Link>
-          <Link to='/about'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+          <Link to='/about' className='text-gray-300 hover:underline'>
+            <li>About</li>
           </Link>
-          <Link to='/sign-in'>
-          <li className='text-slate-700 hover:underline'>Sign in</li>
+          <Link to='/profile'>
+            {currentUser ? (
+              <img 
+                className='rounded-full h-7 w-7 object-cover' 
+                src={currentUser.avatar} 
+                alt='profile'
+              />
+            ) : (
+              <li className='text-gray-300 hover:underline'>Sign in</li>   
+            )}
           </Link>
         </ul>
-        </div>
-      </header>
-  )
+      </div>
+    </header>
+  );
 }
